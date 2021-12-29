4aea616f3d4a38a047ab7a52bcf237c131c7bec7
ftapioca <ftapioca@gmail.com>
2021-12-28 23:07:51 -0300

    tablas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // Creacion de Arreglos con Objetos
 
 var radio = [
-    {HORA: `11:00`,ESPECIALISTA: `Ignacio Schulz`,PACIENTE: `Francisca Rojas`,RUT: `9878782-1`,PREVISION: `FONASA`},
-    {HORA: `11:30`,ESPECIALISTA: `Federico Subercaseaux`,PACIENTE: `Pamela Estrada`,RUT: `15345241-3`,PREVISION: `ISAPRE`},
-    {HORA: `15:00`,ESPECIALISTA: `Fernando Wurthz`,PACIENTE: `Armando Luna`,RUT: `16445345-9`,PREVISION: `ISAPRE`},
-    {HORA: `15:30`,ESPECIALISTA: `Ana Maria Godoy`,PACIENTE: `Manuel Godoy`,RUT: `17666419-0`,PREVISION: `FONASA`},
-    {HORA: `16:00`,ESPECIALISTA: `Patricia Suazo`,PACIENTE: `Ramon Ulloa`,RUT: `14989389-K`,PREVISION: `FONASA`}
+    {HORA: `11:00`,ESPECIALISTA: `IGNACIO SCHULZ`,PACIENTE: `FRANCISCA ROJAS`,RUT: `9878782-1`,PREVISION: `FONASA`},
+    {HORA: `11:30`,ESPECIALISTA: `FEDERICO SUBERCASEAUX`,PACIENTE: `PAMELA ESTRADA`,RUT: `15345241-3`,PREVISION: `ISAPRE`},
+    {HORA: `15:00`,ESPECIALISTA: `FERNANDO WURTHZ`,PACIENTE: `ARMANDO LUNA`,RUT: `16445345-9`,PREVISION: `ISAPRE`},
+    {HORA: `15:30`,ESPECIALISTA: `ANA MARIA GODOY`,PACIENTE: `MANUEL GODOY`,RUT: `17666419-0`,PREVISION: `FONASA`},
+    {HORA: `16:00`,ESPECIALISTA: `PATRICIA SUAZO`,PACIENTE: `RAMON ULLOA`,RUT: `14989389-K`,PREVISION: `FONASA`}
 ];
 
 var trauma = [
@@ -29,99 +29,144 @@ var dental = [
 
 
 
-
 //Div Contenedor
 document.write("<div class='container'>");
-    //Título H1
+//Título H1
     document.write("<table class='table table-borderless mb-0 mt-2'>");
         document.write("<tr>");
-            document.write("<td class='p-0'><h1>Centor Médico Ñuñoa</h1></td>");
+            document.write("<td class='p-0'><h1>Centro Médico Ñuñoa</h1></td>");
         document.write("</tr>");
     document.write("</table>");
 
-    //Tabla primera y ultima atencion
+//Tabla primera y ultima atencion
     document.write("<table class='table table-borderless p-0'>");
         document.write("<tr>");
-        // Título
+    // Título
             document.write("<td class='p-0'><span class='font-weight-bold'><h4>Primera y última atención por especialidad<h4></br></span></td>");
         document.write("</tr>");
 
-        // Especialidad Radiologia
+    // Especialidad Radiologia
         document.write("<tr>");
             document.write("<td class='p-0'><span class='font-weight-regular'><h5>Radiología:</h5>Primera atención: "+ radio[0].PACIENTE + " - " + radio[0].PREVISION + " | Última atención: " + radio[radio.length-1].PACIENTE + " - " + radio[radio.length-1].PREVISION +  "</span></td>");
         document.write("</tr>");
         
-        // Especialidad Traumatologia
+    // Especialidad Traumatologia
         document.write("<tr>");
             document.write("<td class='p-0'><span class='font-weight-regular'><h5></br>Traumatología:</h5>Primera atención: "+ trauma[0].PACIENTE + " - " + trauma[0].PREVISION + " | Última atención: " + trauma[trauma.length-1].PACIENTE + " - " + trauma[trauma.length-1].PREVISION +  "</span></td>");
         document.write("</tr>");
         document.write("</tr>");
 
-        // Especialidad Dental
+    // Especialidad Dental
         document.write("<tr>");
             document.write("<td class='p-0'><span class='font-weight-regular'><h5></br>Dental:</h5>Primera atención: "+ dental[0].PACIENTE + " - " + dental[0].PREVISION + " | Última atención: " + dental[dental.length-1].PACIENTE + " - " + dental[dental.length-1].PREVISION +  "</span></td>");
         document.write("</tr>");
 
-    //Creación de Tabla Notas
+
+
+//Creación de Tablas con atenciones por especialidad
+
+//Tabla de Radiologia
+    document.write("<table class='table table-borderless mb-0 mt-2'>");
+    document.write("<tr>");
+    document.write("<td class='p-0'><h2></br>Atenciones Radiología</h2></td>");
+    document.write("</tr>");
+    document.write("</table>");
     document.write("<table class='table table-hover'>");
-
-        //Cabecera Tabla Notas
+    //Cabecera Tabla radio
         document.write("<thead class='bg-dark text-light'>");
+    //Columnas Cabecera Tabla radio
+        document.write("<tr>");
+        //Contenido Columnas Cabecera Tabla radio
+            document.write("<th scope='col'>Hora</th>");
+            document.write("<th scope='col' class='text-center'>Especialista</th>");
+            document.write("<th scope='col' class='text-center'>Paciente</th>");
+            document.write("<th scope='col' class='text-center' >RUT</th>");
+            document.write("<th scope='col' class='text-center'>Previsión</th>");
+            document.write("</tr>"); //Cierro Columnas Cabecera Tabla radio
+            document.write("</thead>"); //Cierre Cabecera Tabla radio
 
-            //Columnas Cabecera Tabla Notas
-            document.write("<tr>");
+            //Cuerpo Tabla atenciones Radio
+            document.write("<tbody>");
+                for (let i = 0; i<radio.length ; i++) {
+                    document.write("<tr>");
+                    document.write("<th scope='row' class='text-center'>"+ radio[i].HORA +"</th>");
+                    document.write("<td scope='row' class='text-center'>"+ radio[i].ESPECIALISTA +"</td>");
+                    document.write("<td scope='row' class='text-center'>"+ radio[i].PACIENTE +"</td>");
+                    document.write("<td scope='row' class='text-center'>"+ radio[i].RUT +"</td>");
+                    document.write("<td scope='row' class='text-center'>"+ radio[i].PREVISION +"</td>");
+                    document.write("</tr>");
+                }
+// Fin Tabla Radio
 
-                //Contenido Columnas Cabecera Tabla Notas
-                document.write("<th scope='col'>Hora</th>");
-                document.write("<th scope='col' class='text-center'>Especialista</th>");
-                document.write("<th scope='col' class='text-center'>Paciente</th>");
-                document.write("<th scope='col' class='text-center' >RUT</th>");
-                document.write("<th scope='col' class='text-center'>Previsión</th>");
-            document.write("</tr>"); //Cierro Columnas Cabecera Tabla Notas
-        document.write("</thead>"); //Cierre Cabecera Tabla Notas
+//Tabla de Traumatología
+    document.write("<table class='table table-borderless mb-0 mt-2'>");
+    document.write("<tr>");
+    document.write("<td class='p-0'><h2></br>Atenciones Traumatología</h2></td>");
+    document.write("</tr>");
+    document.write("</table>");
+    document.write("<table class='table table-hover'>");
+    document.write("<table class='table table-hover'>");
+    //Cabecera Tabla radio
+        document.write("<thead class='bg-dark text-light'>");
+    //Columnas Cabecera Tabla radio
+        document.write("<tr>");
+        //Contenido Columnas Cabecera Tabla radio
+            document.write("<th scope='col'>Hora</th>");
+            document.write("<th scope='col' class='text-center'>Especialista</th>");
+            document.write("<th scope='col' class='text-center'>Paciente</th>");
+            document.write("<th scope='col' class='text-center' >RUT</th>");
+            document.write("<th scope='col' class='text-center'>Previsión</th>");
+            document.write("</tr>"); //Cierro Columnas Cabecera Tabla radio
+            document.write("</thead>"); //Cierre Cabecera Tabla radio
 
-        //Cuerpo Tabla Notas
-        document.write("<tbody>");
+            //Cuerpo Tabla atenciones trauma
+            document.write("<tbody>");
+                for (let i = 0; i<trauma.length ; i++) {
+                    document.write("<tr>");
+                    document.write("<th scope='row' class='text-center'>"+ trauma[i].HORA +"</th>");
+                    document.write("<td scope='row' class='text-center'>"+ trauma[i].ESPECIALISTA +"</td>");
+                    document.write("<td scope='row' class='text-center'>"+ trauma[i].PACIENTE +"</td>");
+                    document.write("<td scope='row' class='text-center'>"+ trauma[i].RUT +"</td>");
+                    document.write("<td scope='row' class='text-center'>"+ trauma[i].PREVISION +"</td>");
+                    document.write("</tr>");
+                }
+// Fin Tabla traumatologia
 
-            //Filas Cuerpo Tabla Notas
+//Tabla de Dental
+    document.write("<table class='table table-borderless mb-0 mt-2'>");
+    document.write("<tr>");
+    document.write("<td class='p-0'><h2></br>Atenciones Dental</h2></td>");
+    document.write("</tr>");
+    document.write("</table>");
+    document.write("<table class='table table-hover'>");
+    document.write("<table class='table table-hover'>");
+    //Cabecera Tabla radio
+        document.write("<thead class='bg-dark text-light'>");
+    //Columnas Cabecera Tabla radio
+        document.write("<tr>");
+        //Contenido Columnas Cabecera Tabla radio
+            document.write("<th scope='col'>Hora</th>");
+            document.write("<th scope='col' class='text-center'>Especialista</th>");
+            document.write("<th scope='col' class='text-center'>Paciente</th>");
+            document.write("<th scope='col' class='text-center' >RUT</th>");
+            document.write("<th scope='col' class='text-center'>Previsión</th>");
+            document.write("</tr>"); //Cierro Columnas Cabecera Tabla radio
+            document.write("</thead>"); //Cierre Cabecera Tabla radio
 
-            //Fila ramo 1 Tabla Notas
-            document.write("<tr>");
-                //Datos Fila ramo 1 Tabla Notas
-                document.write("<th scope='row'>"+ r1 +"</th>");
-                document.write("<td class='text-center'>"+ n1_r1 +"</td>");
-                document.write("<td class='text-center'>"+ n2_r1 +"</td>");
-                document.write("<td class='text-center'>"+ n3_r1 +"</td>");
-                document.write("<td class='text-center font-weight-bold'>"+ prom_r1.toFixed(2) +"</td>");
-            document.write("</tr>"); //Cierre Fila ramo 1 Tabla Notas
+            //Cuerpo Tabla atenciones dental
+            document.write("<tbody>");
+                for (let i = 0; i<dental.length ; i++) {
+                    document.write("<tr>");
+                    document.write("<th scope='row' class='text-center'>"+ dental[i].HORA +"</th>");
+                    document.write("<td scope='row' class='text-center'>"+ dental[i].ESPECIALISTA +"</td>");
+                    document.write("<td scope='row' class='text-center'>"+ dental[i].PACIENTE +"</td>");
+                    document.write("<td scope='row' class='text-center'>"+ dental[i].RUT +"</td>");
+                    document.write("<td scope='row' class='text-center'>"+ dental[i].PREVISION +"</td>");
+                    document.write("</tr>");
+                }
+// Fin Tabla dental
 
-            //Fila ramo 2 Tabla Notas
-            document.write("<tr>");
-                //Datos Fila ramo 2 Tabla Notas
-                document.write("<th scope='row'>"+ r2 +"</th>");
-                document.write("<td class='text-center'>"+ n1_r2 +"</td>");
-                document.write("<td class='text-center'>"+ n2_r2 +"</td>");
-                document.write("<td class='text-center'>"+ n3_r2 +"</td>");
-                document.write("<td class='text-center font-weight-bold'>"+ prom_r2.toFixed(2) +"</td>");
-            document.write("</tr>"); //Cierre Fila ramo 2 Tabla Notas
 
-            //Fila ramo 3 Tabla Notas
-            document.write("<tr>");
-                //Datos Fila ramo 3 Tabla Notas
-                document.write("<th scope='row'>"+ r3 +"</th>");
-                document.write("<td class='text-center'>"+ n1_r3 +"</td>");
-                document.write("<td class='text-center'>"+ n2_r3 +"</td>");
-                document.write("<td class='text-center'>-</td>");
-                document.write("<td class='text-center font-weight-bold'>-</td>");
-            document.write("</tr>"); //Cierre Fila ramo 3 Tabla Notas
-        document.write("</tbody>"); //Cierro Cuerpo Tabla Notas
-    document.write("</table>"); //Cierro Tabla Notas
-
-    //Mensajes de nota necesaria para aprobar
-
-    if (n3_aprob<=10) {
-        document.write("<p>Para aprobar el ramo <span class='font-weight-bold'>"+ r3 +"</span> , debes obtener como mínimo un <span class='font-weight-bold'>"+ n3_aprob + "</span> en tu tercera nota.")
-    } else {
-        document.write("<p>Para aprobar el ramo <span class='font-weight-bold'>"+ r3 +"</span> , debes obtener como mínimo un <span class='font-weight-bold'>"+ n3_aprob + "</span> en tu tercera nota,  <span class='font-weight-bold text-uppercase'>y eso es imposible...</span>")
-    }
+        document.write("</tbody>"); //Cierro Cuerpo Tabla 
+    document.write("</table>"); //Cierro Tabla 
 document.write("</div>");//Cierre Contenedor
